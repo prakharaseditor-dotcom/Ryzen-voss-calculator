d7b15fb628a87b77a11a6647d620dfc7cc9dd7a5
Prakhar <prakharaseditor@gmail.com>
2026-07-18 16:58:14 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,24 @@
-const CACHE_NAME = "ryzen-voss-v1";
+const CACHE_NAME = "ryzen-voss-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
